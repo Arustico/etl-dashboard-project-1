5dd203f317224e5427d0687de89453cec5ccdd46
Ariel Nuñez <ariel.nunez.sa@gmail.com>
2026-02-05 00:50:39 -0300

    Actualización de transformation.py, transforma headers, y filtra columnas útiles

--- src/transformation.py
+++ src/transformation.py
@@ -1,11 +1,24 @@
 #%% LIBRERIAS
-import pandas
+import pandas as pd
 import logging
-
-
 from transform_headers import transform_headers_main
 
+sep = "="*30
+logging.info(f"TRANSFORMACIÓN DE ENCABEZADO...\n{sep}")
 data = transform_headers_main()
 
-print(data)
+logging.info(f"FILTRO DE COLUMNAS ÚTILES...\n{sep}")
+unified_colnames = [list(df.keys()) for df in data]
+unified_colnames = list(
+    set.intersection(*map(set, unified_colnames))
+    )
+unified_colnames = [col for col in unified_colnames if (("Unnamed" not in col)and("key_" not in col))]
+#print(unified_colnames)
+
+newdata = []
+for df in data:
+    df2 = df[unified_colnames]
+    newdata.append(df2)
+newdata = pd.concat(newdata)
+print(newdata)
 
